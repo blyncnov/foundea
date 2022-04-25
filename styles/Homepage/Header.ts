37f7bcc156437ed2a1af6473e77e9c22b562241e
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-04-25 01:31:56 +0100

    foundae kick start Hero started v4

--- styles/Homepage/Header.ts
+++ styles/Homepage/Header.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
-    height: 90vh;
     width: 100%;
     background: linear-gradient(193deg,#ffffff 70%,#DFD9F8 100%);
 `
